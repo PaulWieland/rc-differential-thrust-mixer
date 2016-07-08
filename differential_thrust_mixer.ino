3cc6b3d2aea6d59a56f70dcf153ffb25d373266c
fahrvergnuugen <p@ul.sickdimension.com>
2016-07-08 14:04:01 -0400

    Behaves more like a programable RC transmitter & added WS2812 driver
    Added a mix strength setting
    Added min & max commands to be sent to ESCs
    Changed formula for calculating mix - yawing now increases one more and decreases the other motor. Previously it only decreased on motor.
    Added support for WS2812 navigation lights

--- differential_thrust_mixer.ino
+++ differential_thrust_mixer.ino
@@ -1,16 +1,33 @@
 #include <Servo.h>
+#include <Adafruit_NeoPixel.h>
 
 int throttle, yaw, modifyer = 1000;
 float yaw_pct;
-byte throttle_pin = 2;
-byte yaw_pin = 3;
-byte l_motor_pin = 9;
-byte r_motor_pin = 10;
 
-int reverse_yaw = -1; // 1 or -1 to reverse the yaw channel
-int yaw_deadband = 20; // ignore this much jitter on yaw channel
+const int min_command = 1000;
+const int max_command = 2000;
+
+
+const byte throttle_pin = 2;
+const byte yaw_pin = 3;
+const byte l_motor_pin = 9;
+const byte r_motor_pin = 10;
+
+const int reverse_yaw = -1; // 1 or -1 to reverse the yaw channel
+const int yaw_deadband = 20; // ignore this much jitter on yaw channel
+const int mix_strength = .5; // value from 0 (no mix) to 1 (100%) for the mix strength. May eventually set this value from another switch/channel on the receiver
 
 Servo l_motor, r_motor;
+
+/* Optional WS2812 LED Driver for marker strobes */
+#define led_pin 4
+#define num_pixels 6
+Adafruit_NeoPixel pixels = Adafruit_NeoPixel(num_pixels, led_pin, NEO_GRB + NEO_KHZ800);
+int framecounter = 0;
+int frames_to_wait = 1000; // number of loops to wait before flashing the strobes
+
+//int last_g_flash = 0;
+//int last_r_flash = 0;
 
 
 void setup() {
@@ -21,14 +38,26 @@ void setup() {
 
   l_motor.attach(l_motor_pin);
   r_motor.attach(r_motor_pin);
+
+
+/* Optional WS2812 LEDs */
+  pixels.begin();
+  pixels.setPixelColor(0, pixels.Color(0,255,0));
+  pixels.setPixelColor(1, pixels.Color(0,255,0));
+  pixels.setPixelColor(2, pixels.Color(255,0,0));
+  pixels.setPixelColor(3, pixels.Color(255,0,0));
+  pixels.setPixelColor(4, pixels.Color(175,175,175));
+  pixels.setPixelColor(5, pixels.Color(175,175,175));
+
+
   
   Serial.begin(115200);
 }
 
 void loop() {
+  framecounter++;
   // put your main code here, to run repeatedly:
-//  delay(100);
-  
+
   // read input pwm from throttle and yaw
   throttle = pulseIn(throttle_pin, HIGH);
   yaw = pulseIn(yaw_pin,HIGH);
@@ -41,24 +70,50 @@ void loop() {
   }
   
   // calculate transformation
-  modifyer = abs(yaw_pct) * (throttle - 1000);
+  modifyer = abs(yaw_pct) * (throttle - 1000) * mix_strength;
 //  Serial.println("yaw pct"+(String) yaw_pct);
   
   // calculate & write left and right motor mixed throttle/yaw values
   if(yaw_pct < 0){
-    l_motor.writeMicroseconds(throttle - modifyer);
-    r_motor.writeMicroseconds(throttle);
+    l_motor.writeMicroseconds((throttle - modifyer) < min_command ? min_command : (throttle - modifyer));
+    r_motor.writeMicroseconds((throttle + modifyer) > max_command ? max_command : (throttle + modifyer));
 //    Serial.println("yaw left: "+ (String) (modifyer));
   }else{
-    l_motor.writeMicroseconds(throttle);
-    r_motor.writeMicroseconds(throttle - modifyer);
+    l_motor.writeMicroseconds((throttle + modifyer) > max_command ? max_command : (throttle + modifyer));
+    r_motor.writeMicroseconds((throttle - modifyer) < min_command ? min_command : (throttle - modifyer));
 //    Serial.println("yaw right: "+ (String) (modifyer));
   }
 
   String t;
   Serial.println("T: "+(String) throttle + "  Y: "+ (String) yaw + "  LM: " + (String) l_motor.readMicroseconds() + "  RM: "+ r_motor.readMicroseconds());
-}
 
+
+/* Strobe pattern
+<framestowait>  <framestowait>  <framestowait>
+          <strobe L>       <strobe R>
+--------------||--------------||--------------
+*/
+  if(framecounter == frames_to_wait || framecounter == frames_to_wait+100){
+    pixels.setPixelColor(0, pixels.Color(255,255,255));
+    pixels.setPixelColor(1, pixels.Color(255,255,255));
+  }else{
+    pixels.setPixelColor(0, pixels.Color(0,255,0));
+    pixels.setPixelColor(1, pixels.Color(0,255,0));    
+  }
+
+  if(framecounter == frames_to_wait*2 || framecounter == (frames_to_wait*2+100)){
+    pixels.setPixelColor(2, pixels.Color(255,255,255));
+    pixels.setPixelColor(3, pixels.Color(255,255,255));
+  }else{
+    pixels.setPixelColor(2, pixels.Color(255,0,0));
+    pixels.setPixelColor(3, pixels.Color(255,0,0));    
+  }
+
+  if(framecounter > frames_to_wait*3){
+    framecounter = 0;
+  }
+  
+}
 
 
 
